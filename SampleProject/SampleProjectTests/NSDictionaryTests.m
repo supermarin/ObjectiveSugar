3b4ff6fc518e38fda8829a56c61dee5f1af3e984
Marin Usalj <mneorr@gmail.com>
2012-11-23 18:13:55 -0800

    Added NSDictionary iterators

---
+++ SampleProject/SampleProjectTests/NSDictionaryTests.m
@@ -0,0 +1,60 @@
+//
+//  NSDictionaryTests.m
+//  SampleProject
+//
+//  Created by Marin Usalj on 11/23/12.
+//  Copyright (c) 2012 @mneorr | mneorr.com. All rights reserved.
+//
+
+#import "NSDictionaryTests.h"
+#import "Kiwi.h"
+#import "NSDictionary+Rubyfy.h"
+
+@implementation NSDictionaryTests
+@end
+
+SPEC_BEGIN(Rubify)
+
+describe(@"Iterators", ^{
+    
+    NSDictionary *sampleDict = @{
+        @"one" : @1,
+        @"two" : @2,
+        @"three" : @3
+    };
+    __block int counter;
+    
+    beforeEach(^{
+        counter = 0;
+    });
+    
+    afterEach(^{
+        [[@(counter) should] equal:@(sampleDict.allKeys.count)];
+    });
+    
+    
+    it(@"iterates each key and value", ^{
+        [sampleDict each:^(id key, id value) {
+            [[sampleDict.allKeys[counter] should] equal:key];
+            [[sampleDict.allValues[counter] should] equal:value];
+            counter ++;
+        }];
+    });
+    
+    it(@"iterates all keys", ^{
+        [sampleDict eachKey:^(id key){
+            [[sampleDict.allKeys[counter] should] equal:key];
+            counter ++;
+        }];
+    });
+    
+    it(@"iterates all values", ^{ 
+        [sampleDict eachValue:^(id value){
+            [[sampleDict.allValues[counter] should] equal:value];
+            counter ++;
+        }];  
+    });
+    
+});
+
+SPEC_END
