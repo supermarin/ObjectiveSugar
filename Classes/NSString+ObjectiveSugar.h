c1cd4cf2b1999caa067676c87de5be10b0aea741
Vasilis Akoinoglou <alladinian@gmail.com>
2013-10-14 11:57:30 +0300

    Documentation comments for NSString additions. Imported NSArray additions to NSSet in order to assure the compiler about NSArray's '-sample' method.

--- Classes/NSString+ObjectiveSugar.h
+++ Classes/NSString+ObjectiveSugar.h
@@ -12,14 +12,42 @@ NSString *NSStringWithFormat(NSString *format, ...) NS_FORMAT_FUNCTION(1,2);
 
 @interface NSString(ObjectiveSugar)
 
+/**
+ Returns an array containing substrings from the receiver that have been divided by a whitespace delimiter
+ 
+ @return An array containing substrings that have been divided by a whitespace delimiter
+ */
 - (NSArray *)split;
+
+
+/**
+ Returns an array containing substrings from the receiver that have been divided by a given delimiter
+ 
+ @param delimiter The delimiter string
+ @return An array containing substrings that have been divided by delimiter
+ */
 - (NSArray *)split:(NSString *)delimiter;
 
+
+/**
+ Returns a new string made by converting a snake_case_string to CamelCaseString
+ 
+ @return A string made by converting a snake_case_string to CamelCaseString
+ */
 - (NSString *)camelCase;
+
+
+/**
+ Returns a Boolean value that indicates whether a given string is a substring of the receiver
+ 
+ @return YES if 'string' is a substring of the receiver, otherwise NO
+ */
 - (BOOL)containsString:(NSString *)string;
+
 
 /**
  Returns a new string made by removing whitespaces and newlines from both ends of the receiver
+ 
  @return A string without trailing or leading whitespaces and newlines
  */
 - (NSString *)strip;
